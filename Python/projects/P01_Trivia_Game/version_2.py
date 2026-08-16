03aada7f69f4c64af40cbc92fe1e9e87f8760839
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-16 22:15:57 +0300

    update

--- Python/projects/P01_Trivia_Game/version_2.py
+++ Python/projects/P01_Trivia_Game/version_2.py
@@ -1,7 +1,22 @@
 
+# Functional requirements
+    #  Store multiple questions.
+    #  Each question should have multiple answer choices.
+    #  Display one question at a time.
+    #  Display the available choices.
+    #  Accept the user's answer.
+    #  Determine whether the answer is correct.
+    #  Increase the score when the answer is correct.
+    #  Move to the next question.
+    #  Display the final score.
 
-
-questions = {
-    "question": "What is the correct way to print 'Hello' in Python?"
-    
-}
+# questions = [
+#     {
+#         "question": "What is the correct way to print 'Hello' in Python?",
+#         "options": ['echo("Hello")', 'print("Hello")', 'printf("Hello")', 'console.log("Hello")'],
+#         "correct_answer": ""
+#     }
+#     {
+#         ""
+#     }
+# ]
