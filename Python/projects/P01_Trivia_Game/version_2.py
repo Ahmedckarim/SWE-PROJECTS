af06e380cfae5ee13df48ecf1634cd04e3c9c87c
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-17 21:35:36 +0300

    Ongoing

--- Python/projects/P01_Trivia_Game/version_2.py
+++ Python/projects/P01_Trivia_Game/version_2.py
@@ -10,13 +10,20 @@
     #  Move to the next question.
     #  Display the final score.
 
-# questions = [
-#     {
-#         "question": "What is the correct way to print 'Hello' in Python?",
-#         "options": ['echo("Hello")', 'print("Hello")', 'printf("Hello")', 'console.log("Hello")'],
-#         "correct_answer": ""
-#     }
-#     {
-#         ""
-#     }
-# ]
+questions = [
+    {
+        "question": "What is the correct way to print 'Hello' in Python?",
+        "options": ['echo("Hello")', 'print("Hello")', 'printf("Hello")', 'console.log("Hello")'],
+        "correct_answer": 'print("Hello")',
+    }
+    {
+        "question": "Which data type is used to store whole numbers?",
+        "options": ["str", "float", "int", "bool"],
+        "correct_answer": "int",
+    }
+    {
+        "question": "What does input() return by default?",
+        "options": ["An integer", "A string", "A float", "A Boolean"]
+        "correct_answer": "A string"
+    }
+]
