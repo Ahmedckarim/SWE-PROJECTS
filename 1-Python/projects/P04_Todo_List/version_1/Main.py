89c7c9f587e774d47b13ab75fffe0401bed778eb
unknown <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-20 14:32:23 +0300

    update

--- 1-Python/projects/P04_Todo_List/version_1/Main.py
+++ 1-Python/projects/P04_Todo_List/version_1/Main.py
@@ -6,21 +6,40 @@
 # Mark task as completed
 # Exit
 
-tasks = ["cooking", "reading"]
-complete = " [Complete]"
 
+
+complete = " [Complete]"
+file_name =  "tasks.txt"
 def menu():
-    print("=== To-Do List Manager ===")
+    print("\n=== To-Do List Manager ===")
     print("1.View tasks")
     print("2. Add a task")
     print("3.Mark task as completed")
     print("4. Delete a task")
     print("5. Exit")
 
-
-
+def load_task():
+    try:
+        with open(file_name, "r") as file:
+            if task:
+                for num,task in enumerate(file_name , start= 1):
+                    print(f"{num}:{task}")
+            else:
+                print('No tasks available to show.')
+    except FileNotFoundError:
+        print("This file name does't exist.")
+    
+def save_task(tasks):
+    try:
+        with open(file_name, "a") as file:
+            for num, task in enumerate(tasks, start= 1):
+                file.write(f"{num}: {task}")
+                print("Task saved succesfully.")
+    except:
+        print("Failed to save.")
 
 def view_tasks(tasks):
+    load_task()
     if not tasks:
         print("There are no tasks yet.")
     else:
@@ -35,10 +54,11 @@ def  add_task(tasks):
     if task:
         tasks.append(task)
         print(f"{task}. Added successfully")
+        save_task(tasks)
     else:
         print("Task can not be empty")
 def mark_task(tasks):
-    view_tasks(tasks)
+    view_tasks()
     try:
         task_number = int(input("Enter the task number: ")) -1
         if 0 <= task_number <= len(tasks):
@@ -69,6 +89,7 @@ def delete_task(tasks):
         print("Please enter a valid number.")
 
 def main():
+    tasks = load_task()
     while True:
         menu()
         choice = input("Enter your Choice: ")
