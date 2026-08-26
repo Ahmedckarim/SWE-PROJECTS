2cf05e150f0ac9e88c019ef4cfc5593a5d76b048
unknown <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-26 21:07:01 +0300

    update

---
+++ 1-Python/fundamentals/founditions/Lesson_ examples.py
@@ -0,0 +1,26 @@
+
+# Membership operators= used to test whether a value or variable is found in a sequence 
+#                  ( string, list, tuple,or dictionary )
+ #                   1. in
+#                    2. not in
+
+word = "apple"
+
+letter = input("Guess a letter in the secret word: ")
+
+# # if letter in word:
+# #     print(f"There is a {letter}")
+# # else:
+# #     print(f"{letter} was not found!")
+
+
+# if letter not in word:
+#     print(f"{letter} was not found")
+# else:
+#     print(f"There is a {letter}")
+
+
+
+
+
+# if name == 'main': 
