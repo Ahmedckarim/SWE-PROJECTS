442908f6965f4f3b241a886376861a91ce7fcab6
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-28 22:01:49 +0300

    updating expense tracker

--- 1-Python/Python-Projects/P06_Expense Tracker/main.py
+++ 1-Python/Python-Projects/P06_Expense Tracker/main.py
@@ -14,6 +14,7 @@ import csv
 json_file_name = "Expenses.json"
 
 def menu():
+    print("\n=== EXPENSE TRACKER === ")
     print("1. Add expense")
     print("2. List expenses")
     print("3. Update expense")
@@ -41,9 +42,9 @@ def add_expense(expenses):
     try:
         expense_id = int(input("Enter expense ID: "))
 
-        # if any(expense["ID"] == expense_id for expense in expenses):
-        #     print("This ID already exist.")
-        #     return
+        if any(expense["ID"] == expense_id for expense in expenses):
+            print("This ID already exist.")
+            return
         
         description = input("Enter a description: ")
         if not description:
@@ -71,19 +72,63 @@ def add_expense(expenses):
  
 def view_all_expenses(expenses):
     if expenses:
+        print("\n Your Expenses")
         for i,expense in enumerate(expenses, start= 1):
             print(f"ID:{expense['ID']} Description:{expense["description"]} Amount: {expense["Amount"]}")
+    else:
+        print("There are no expenses yet")
 
 
 
+def update_expense(expenses):
+    view_all_expenses(expenses)
+    try:
+        expense_id = int(input("Enter the expense ID: "))
+        for expense in expenses:
+            if expense["ID"] == expense_id:
+                print("\nEnter the Updated expense.")
+                descripstion = input("Enter the description: ")
+                if not descripstion:
+                    print("Please enter a description.")
+                    return
+                amount = int(input("Enter an amount: "))
+                if amount <= 0:
+                    print("Amount can not be zero or negative number.")
 
-def update_expense():
-    pass
-def delete_expense():
-    pass
-def view_sammury():
-    pass
+                expense["description"] = descripstion
+                expense["Amount"] = amount
 
+                save_expense(expenses)
+                print("Expense updated successfully.")
+                return
+        print("Expense not found.")
+    except ValueError:
+        print("Please enter number where required.s")
+
+
+
+def delete_expense(expenses):
+    view_all_expenses(expenses)
+    try:
+        expense_id = int(input("Enter the expense Id: "))
+        for expese in expenses:
+            if expese["ID"] == expense_id:
+                expenses.remove(expese)
+
+                save_expense(expenses)
+                print("Expense deleted succussfully.")
+                return
+        print("Expense not found.")
+    except ValueError:
+        print("Please enter a valid ID.")
+
+def view_sammury(expenses):
+    if expenses:
+        total = sum(expense["Amount"] for expense in expenses)
+        print(f"Total expenses: ${total}")
+    else:
+        print("There are no expenses yet")
+        
 def main():
     expenses = load_expense()
     
