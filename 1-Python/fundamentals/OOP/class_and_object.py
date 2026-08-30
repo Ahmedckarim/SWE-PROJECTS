b36ecbac1504d934f15b4cbd1a1ecd79419f92fd
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-08-30 21:56:05 +0300

    Lesson Classes and Objects

--- 1-Python/fundamentals/OOP/class_and_object.py
+++ 1-Python/fundamentals/OOP/class_and_object.py
@@ -1,2 +1,27 @@
-# Classes and Objects 
+# Lesson Classes and Objects 
 
+# In Python, classes and objects work hand in hand to organize
+#    and manage data. You build a class to define shared behavior, 
+#   then create objects that use those behaviors.
+
+
+
+# E.g
+
+class Vecicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def moves(self):
+        print("Moves along..")
+
+    def get_make_model(self):
+        print(f"I'm {self.make} {self.model}.")
+
+my_car = Vecicle("Tesla", "model 3")
+
+# print(my_car.make)
+# print(my_car.model)
+my_car.get_make_model()
+my_car.moves()
