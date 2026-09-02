dcaeb2573826cc732ad23744f94b6cbaddeb9d3e
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-09-02 20:48:34 +0300

    oop toturial project

--- 1-Python/fundamentals/OOP/OOP tourial projects/Acounts.py
+++ 1-Python/fundamentals/OOP/OOP tourial projects/Acounts.py
@@ -8,3 +8,9 @@ Dave.getBalance()
 Sara.getBalance()
 
 Sara.deposit(500)
+Dave.withdraw(10000)
+Dave.withdraw(10)
+
+
+Dave.transfer(10000, Sara)
+Dave.transfer(100, Sara)
--- 1-Python/fundamentals/OOP/OOP tourial projects/bank_count.py
+++ 1-Python/fundamentals/OOP/OOP tourial projects/bank_count.py
@@ -1,5 +1,6 @@
 # 
-
+class BalanceExeption(Exception):
+    pass
 
 
 class BankAccount:
@@ -15,7 +16,37 @@ class BankAccount:
     def deposit(self, amount):
         self.balance = self.balance + amount
         print("\n Deposit complete.")
-        self.getBalance
+        self.getBalance()
+
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceExeption(f"Sorry, account '{self.name}' only has a Balance of ${self.balance:.2f}")
+
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\n withdraw complete.")
+            self.getBalance()
+        except BalanceExeption as error:
+            print(f"\nWithdraw interrupted: {error}")
+
+
+    def transfer(self, amount, account):
+        try: 
+            print('\n*********\n\nBeginning Transfer.. 🚀')
+            self.viableTransaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
+            print('\nTransfer complete! ✔️\n\n***********')
+        except BalanceExeption as error:
+            print(f'\n Transfer interrupted. ❌{error}')
+
+
+
+
         
 
 
