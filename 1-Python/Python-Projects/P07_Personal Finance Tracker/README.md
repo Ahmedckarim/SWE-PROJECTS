de2b082be7d00dd83ab391141a19cdb2ce52cea7
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-09-05 21:02:18 +0300

    personal finance tracker

---
+++ 1-Python/Python-Projects/P07_Personal Finance Tracker/README.md
@@ -0,0 +1,16 @@
+# Project  — Personal Finance Tracker
+
+### Use JSON + CSV.
+
+## Features:
+
+* Add income
+* Add expense
+* Categories
+* Monthly summary
+* Total income
+* Total expenses
+* Balance
+* Export CSV
+* Import CSV
+* Persistent JSON storage
---
+++ 1-Python/Python-Projects/P07_Personal Finance Tracker/main.py
@@ -0,0 +1,36 @@
+
+def menu():
+    print("1. Add Income")
+    print("2. Add Expense")
+    print("3. View Transactions")
+    print("4. View Categories")
+    print("5. Monthly Summary")
+    print("6. Total Income")
+    print("7. Total Expenses")
+    print("8. Current Balance")
+    print("9. Export to CSV")
+    print("10. Import from CSV")
+    print("11. Exit")
+
+
+def add_income():
+    pass
+def add_expense():
+    pass
+def view_transtions():
+    pass
+def view_Categories():
+    pass
+def Monthly_Summary():
+    pass
+def Total_Income():
+    pass
+def Total_Expenses():
+    pass
+def Current_Balance():
+    pass
+def Export_to_CSV():
+    pass
+def Import_from_CSV():
+    pass
+
